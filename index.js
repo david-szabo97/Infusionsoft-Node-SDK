dfbb924bd382af80ffccec24f54e1db31a8a676f
David Szabo <david.szabo97@gmail.com>
2018-04-03 13:44:13 +0200

    Move legacy to entrypoint

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./lib/oauth2')
+const Infusionsoft = require('./lib/oauth2')
+const InfusionsoftLegacy = require('./lib/legacy')
+
+module.exports = () => {
+  return {
+    OAuth2: Infusionsoft,
+    Legacy: InfusionsoftLegacy
+  }
+}
