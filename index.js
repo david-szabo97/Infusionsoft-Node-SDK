f587b4f8d087b965b01388eb68285caea1858a54
David Szabo <david.szabo97@gmail.com>
2018-04-08 15:31:53 +0200

    Return an object instead of a function

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const Infusionsoft = require('./lib/oauth2')
 const InfusionsoftLegacy = require('./lib/legacy')
 
-module.exports = () => {
-  return {
-    OAuth2: Infusionsoft,
-    Legacy: InfusionsoftLegacy
-  }
+module.exports = {
+  OAuth2: Infusionsoft,
+  Legacy: InfusionsoftLegacy
 }
